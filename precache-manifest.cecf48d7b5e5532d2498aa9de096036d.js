8c997fbd70a76220f4ea82b0d737e4b5901fb1a3
ADMIN <NoobieUs37@protonmail.com>
2021-09-05 19:05:51 +0530

    Updates

---
+++ precache-manifest.cecf48d7b5e5532d2498aa9de096036d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f849f545c080c761ad19b4eb83335c",
+    "url": "/labPrograms/index.html"
+  },
+  {
+    "revision": "d7a035da3d0a8af2417f",
+    "url": "/labPrograms/static/js/2.2d265f19.chunk.js"
+  },
+  {
+    "revision": "aa20ada197d25ea5c817",
+    "url": "/labPrograms/static/js/main.df445834.chunk.js"
+  },
+  {
+    "revision": "e4246b214cf32980401d",
+    "url": "/labPrograms/static/js/runtime-main.a301d3b4.js"
+  }
+]);
